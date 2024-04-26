ad8d825eba6a63f876fbfe5485354c64791318e9
Daniel F <dfproffesional@gmail.com>
2024-04-25 21:14:15 -0400

    Update: Components

---
+++ src/views/app/editor.js
@@ -0,0 +1,5 @@
+import HeaderComponent from "./components/header.component.js";
+import PageEditorComponent from "./components/page_editor.component.js";
+
+customElements.define('header-component', HeaderComponent);
+customElements.define('page_editor-component', PageEditorComponent);
